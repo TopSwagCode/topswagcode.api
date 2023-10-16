7885a74eaf729c3da3359a504a84e165314fc4a7
Joshua Ryder <josh@topswagcode.com>
2023-10-16 13:50:06 +0200

    simple file upload

---
+++ Features/File/UploadEndpoint.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+
+namespace TopSwagCode.Api.Features.File;
+
+public class UploadEndpoint : Endpoint<UploadEndpointRequest>
+{
+    public override void Configure()
+    {
+        Post("/upload/image");
+        AllowAnonymous();
+        AllowFileUploads();
+    }
+
+    public override async Task HandleAsync(UploadEndpointRequest req, CancellationToken ct)
+    {
+        if (Files.Count > 0)
+        {
+            var file = Files[0];
+            var length = file.Length;
+            
+            await SendStreamAsync(
+                stream: file.OpenReadStream(),
+                fileName: file.FileName,
+                fileLengthBytes: file.Length,
+                contentType: "image/png");
+
+            return;
+        }
+        await SendNoContentAsync();
+    }
+}
+
+public class UploadEndpointRequest
+{
+    public IFormFile? File { get; set; }
+}
+
+public class Validator : Validator<UploadEndpointRequest>
+{
+    public Validator()
+    {
+        RuleFor(x => x.File)
+            .NotNull().DependentRules(() =>
+            {
+                RuleFor(x => x.File!.ContentType)
+                    .Must(x => x.Equals("image/png", StringComparison.InvariantCultureIgnoreCase))
+                    .WithMessage("Image must be of content type PNG");
+                RuleFor(x => x.File!.Length)
+                    .LessThan(1024 * 512)
+                    .WithMessage("Image must be less than 512kb");
+            });
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # topswagcode.api
 
 Todo:
-* SignalR hubs
+* Add APIKey authentication / Bearer token
+* Add EF Core or Dapper to Postgres
+* SignalR hubs - weatherforecast
 * Event Handlers / Async processing (SignalR ? :D)
 * OpenTelemetry.
 * Unit and Integration Tests. (TestContainers??)
+* Dotnet Core ratelimiting? https://blog.maartenballiauw.be/post/2022/09/26/aspnet-core-rate-limiting-middleware.html
